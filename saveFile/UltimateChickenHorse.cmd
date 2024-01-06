529bd901c20369f5f75aa2e2d71a2662d423bd7d
Imron jehleh <imron.jehleh@gmail.com>
2024-01-06 08:35:34 +0700

    Updated 20240106 T0835
    Still, just normal .bat file. Windows will still warn.

---
+++ saveFile/UltimateChickenHorse.cmd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:49dd173e37b11bd2e6e0e0472d1afcf7e9dd8d6d1d3cbbe6c57e1e437a2ffee2
+size 1164
---
+++ saveFile/ktane.cmd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:20469bd023a2e375893cd560503aa103e8ac24767b417eed853894765c81a611
+size 1160

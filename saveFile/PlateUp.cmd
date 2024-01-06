fd6ea86b478deb6b24ad00752d87429b6b548c2c
Imron jehleh <imron.jehleh@gmail.com>
2024-01-06 07:50:14 +0700

    Updated 20240106 T0749
    Now that works... Seems like you can't have any space between equal sign...

--- saveFile/PlateUp.cmd
+++ saveFile/PlateUp.cmd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:754e3f9580feed5914ba95ec2590e13d8b2347e41a68f3685d054430bb731bdb
-size 1204
+oid sha256:6cca0cfb116c7dff6e67ae02508d8036509ebbc5922b8c1d60c08d8810da8770
+size 1202

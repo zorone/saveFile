c811fa807cb2d4cba4b24dfe46df8f8cece29b2d
Imron jehleh <imron.jehleh@gmail.com>
2024-01-06 07:39:31 +0700

    Updated 20240106 T0739
    Try to debug some codes. Seems like I need to toggles echo on.

--- saveFile/PlateUp.cmd
+++ saveFile/PlateUp.cmd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c375a24f06a8846d541fa0aa92b3c961c4dc8c88073ad8ad51a113589b2cddd2
-size 1071
+oid sha256:8f4c069a1ca8e5737c090f0c3e5c1ab4f411a4e8c0bd0893998dea95904baebd
+size 1171
